6ce2e155ea9bc48a440c37d11cb698d934baf3f8
FiachraSheridan <60360033+FiachraSheridan@users.noreply.github.com>
2020-02-05 17:44:42 +0000

    Exercise Work
    Stuff from class today.
    See Exercise sheet on moodle for instructions to continue

---
+++ New Unity Project/Assets/Scripts/AudioSource2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSource2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource AudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P) && !AudioSource.isPlaying)
+        {
+            AudioSource.Play();
+        }
+        else if (Input.GetKey(KeyCode.P) && AudioSource.isPlaying)
+        {
+            AudioSource.Pause();
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            AudioSource.Pause();
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/TimeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public float elapsedTime, eventTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if(elapsedTime >= eventTime)
+        {
+            AudioSource.Play();
+            elapsedTime = 0;
+        }
+    }
+}
