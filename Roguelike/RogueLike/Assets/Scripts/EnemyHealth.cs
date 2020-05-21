f40c0e517988061d59d475f2cf6f6e6e7c4a1ed0
Fiachra <twin.fiachra@gmail.com>
2020-05-21 01:19:47 +0100

    new

---
+++ Roguelike/RogueLike/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int Health = 20;
+    public int damageMax = 5, damageMin = 2;
+    public GameObject PowerUp;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        
+
+        if (collision.gameObject.tag == "Arrow")
+        {
+            Health -= Random.Range(2, 5);
+        }
+    }
+
+    void Update()
+    {
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Roguelike/RogueLike/Assets/Scripts/WeaponChoice.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponChoice : MonoBehaviour
+{
+
+    public GameObject Sling;
+    public GameObject Wand;
+    public Transform AttachPoint;
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "WandWorld")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Destroy(Sling);
+                Destroy(collision.gameObject);
+
+                Instantiate(Wand, AttachPoint);
+                Wand.transform.position = new Vector3(0.038f, 0.03f, 0);
+            }
+                      
+        }
+    }
+
+
+    
+    
+}
